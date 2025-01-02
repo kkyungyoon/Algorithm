85258e47a56226bde7871ed057d0f87a8ddb4337
kyungyoon <ruddbs803@gmail.com>
2025-01-02 09:07:17 +0900

    [Gold V] Title: 회의실 배정, Time: 364 ms, Memory: 126024 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,6 +1,7 @@
 N = int(input())
 meetings = [tuple(map(int, input().split())) for _ in range(N)]
 
+# 겹치지 않으면서 최대한 많은 회의를 선택하기 위해 끝나는 시간 기준으로 정렬
 meetings.sort(key= lambda x: (x[1], x[0]))
 cnt = 0
 last_end = 0
